6d6d39b6f6eed978f76b34569299f411f6641324
Mees <rasmees123@gmail.com>
2025-03-07 21:34:48 +0100

    Refactor profile & post views

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,7 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
-    
+    like_id = serializers.SerializerMethodField()
+
+
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError(
@@ -26,6 +29,15 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
@@ -34,5 +46,6 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at', 'updated_at', 'title',
             'movie_title', 'movie_image', 'content',
             'movie_image', 'movie_spoilers', 'is_owner',
-            'movie_positives', 'movie_negatives'
+            'movie_positives', 'movie_negatives',
+            'like_id',
         ]
