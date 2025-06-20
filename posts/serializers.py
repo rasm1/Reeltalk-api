2bd6dd91831dc6bed10296d1379ecdcd4219c86b
Mees <rasmees123@gmail.com>
2025-06-20 23:22:34 +0200

    FIX:post serializer to make image optional

--- posts/serializers.py
+++ posts/serializers.py
@@ -14,24 +14,26 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
+    image = serializers.ImageField(required=False, allow_null=True)
+
 
     def validate_image(self, value):
         """
         validator for image size
         """
+        if not value:
+            return value
+
         if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError(
-                'Image is too large (2MB)'
-            )
+            raise serializers.ValidationError('Image is too large (2MB)')
+
         if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width too large'
-            )
+            raise serializers.ValidationError('Image width too large')
+
         if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height too large'
-            )
-        return value
+            raise serializers.ValidationError('Image height too large')
+
+        return value    
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -51,7 +53,7 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'title',
-            'movie_title', 'image', 'content',
+            'movie_title', 'content',
             'image', 'movie_spoilers', 'is_owner',
             'movie_positives', 'movie_negatives',
             'like_id', 'likes_count', 'comments_count'
